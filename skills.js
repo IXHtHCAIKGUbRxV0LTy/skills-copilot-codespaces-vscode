e25c4179327d31fe2d8e9e1ceeea0c33e0e6b3cf
IXHtHCAIKGUbRxV0LTy <cdong@splunk.com>
2025-02-25 06:32:01 +0000

    Copilot commit

---
+++ skills.js
@@ -0,0 +1,10 @@
+/**
+ * Converts a temperature from Fahrenheit to Celsius.
+ *
+ * @param {number} fahrenheit - The temperature in Fahrenheit.
+ * @returns {number} The temperature converted to Celsius.
+ */
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
